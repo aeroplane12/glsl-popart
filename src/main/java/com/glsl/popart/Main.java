4ea646a5ba69cb8e64abf3b74e849f1e06913605
Keita <keita@students.uni-marburg.de>
2025-04-30 17:12:42 +0200

    set window, posterization shader

--- src/main/java/com/glsl/popart/Main.java
+++ src/main/java/com/glsl/popart/Main.java
@@ -1,9 +1,51 @@
 package com.glsl.popart;
 
-public class Main {
+import com.jogamp.opengl.*;
+import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.util.FPSAnimator;
+
+import javax.swing.*;
+
+public class Main implements GLEventListener {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // OpenGL-Profil abrufen
+        GLProfile profile = GLProfile.get(GLProfile.GL2);
+        GLCapabilities capabilities = new GLCapabilities(profile);
+
+        // Canvas mit OpenGL-Kontext erzeugen
+        GLCanvas canvas = new GLCanvas(capabilities);
+        canvas.addGLEventListener(new Main()); // Diese Klasse wird OpenGL-Ereignisse behandeln
+        canvas.setSize(800, 600);
+
+        // Fenster erzeugen
+        JFrame frame = new JFrame("GLSL PopArt Demo");
+        frame.getContentPane().add(canvas);
+        frame.setSize(frame.getContentPane().getPreferredSize());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        // Animator starten (optional für kontinuierliche Darstellung)
+        FPSAnimator animator = new FPSAnimator(canvas, 60, true);
+        animator.start();
     }
+
+    // Wird aufgerufen, wenn das Rendering gestartet wird
+    @Override
+    public void init(GLAutoDrawable drawable) {
+        System.out.println("Hello OpenGL! Fenster ist bereit.");
+    }
+
+    // Wird bei Fenster-Resize aufgerufen
+    @Override
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {}
+
+    // Zeichnen pro Frame
+    @Override
+    public void display(GLAutoDrawable drawable) {}
+
+    // Cleanup, wenn Fenster geschlossen wird
+    @Override
+    public void dispose(GLAutoDrawable drawable) {}
 
 }
--- src/main/resources/shaders/posterization.frag
+++ src/main/resources/shaders/posterization.frag
@@ -1,5 +1,12 @@
 #version 120
 
+uniform sampler2D u_texture;
+uniform float u_levels;
+
+varying vec2 v_texCoord;
+
 void main() {
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    vec4 color = texture2D(u_texture, v_texCoord);
+    color.rgb = floor(color.rgb * u_levels) / u_levels;
+    gl_FragColor = color;
 }
